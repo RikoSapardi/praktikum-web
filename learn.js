7239ee6d3e4275cbaf84e0f282b700f6c5941e9a
RikoSapardi <rikosapardi@student.uir.ac.id>
2022-01-08 21:10:54 +0700

    Latihan 5.4: Melakukan operasi sederhana dengan operator Logika

--- learn.js
+++ learn.js
@@ -39,3 +39,17 @@ console.log("a == b =", a == b);
 console.log("a != b =", a != b);
 console.log("a === b =", a === b);
 console.log("a !== b =", a !== b);
+
+console.log("");
+console.log("true && true =", true && true);
+console.log("true && false =", true && false);
+console.log("false && true =", false && true);
+console.log("false && false =", false && false);
+console.log("true || true =", true || true);
+console.log("true || false =", true || false);
+console.log("false || true =", false || true);
+console.log("false || false =", false || false);
+console.log("!true =", !true);
+console.log("!false =", !false);
+
+console.log("");
