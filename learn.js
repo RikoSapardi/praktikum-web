6b5de783eaa9525289859aa959e7d56c948f97c4
RikoSapardi <rikosapardi@student.uir.ac.id>
2022-01-08 21:14:19 +0700

    Latihan 5.6: Bermain dengan array

--- learn.js
+++ learn.js
@@ -53,3 +53,61 @@ console.log("!true =", !true);
 console.log("!false =", !false);
 
 console.log("");
+function luas_persegi(lebar) {
+  var panjang = 10;
+  var luas = lebar * panjang;
+  console.log(panjang + " + " + lebar + " = " + luas);
+}
+
+function luas_segitiga(alas, tinggi) {
+  var luas = 0.5 * alas * tinggi;
+  console.log("0.5 * " + alas + " * " + tinggi + " = " + luas);
+}
+
+function volume_balok(panjang, lebar, tinggi) {
+  var volume = panjang * lebar * tinggi;
+  console.log(panjang + " * " + lebar + " * " + tinggi + " = " + volume);
+}
+
+luas_persegi(6);
+console.log("Luas Segitiga\n");
+luas_segitiga(5, 12);
+console.log("Volume_Balok\n");
+volume_balok(10, 11, 33);
+
+console.log("");
+var hewan = ["beruang", "Lumba-Lumba", "Semut", "Unta"];
+console.log(hewan[0]);
+console.log(hewan[1]);
+console.log(hewan[2]);
+console.log(hewan[3] + "\n");
+
+hewan[1] = "anjing";
+console.log(hewan);
+
+console.log("\nmethod push");
+hewan.push("ular");
+console.log(hewan);
+
+console.log("\nmethod pop");
+hewan.pop();
+console.log(hewan);
+
+console.log("\nproperty length");
+console.log(hewan.length);
+
+console.log("\nmethod unshift");
+hewan.unshift("kelinci");
+console.log(hewan);
+
+console.log("\nmethod shift");
+hewan.shift();
+console.log(hewan);
+
+console.log("\nmethod short");
+hewan.sort();
+console.log(hewan);
+
+console.log("\nmethod reverse");
+hewan.reverse();
+console.log(hewan);
